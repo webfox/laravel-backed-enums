2630d5f00df0b8c4f1d8f1a8f9af4283519c7fd7
Matthew Hailwood <matthew@webfox.co.nz>
2023-10-07 19:31:17 +1300

    Fix toJson method to actually return a json string instead of an array.
    Add new `AsFullEnumCollection` cast.
    See the readme for usage and a description of why this is useful.

---
+++ src/Casts/AsFullEnumCollection.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace Webfox\LaravelBackedEnums\Casts;
+
+use BackedEnum;
+use LogicException;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use function is_int;
+use function collect;
+use function is_array;
+use function constant;
+use function is_string;
+use function is_subclass_of;
+
+/**
+ * @template T of \UnitEnum|\BackedEnum
+ */
+class AsFullEnumCollection implements CastsAttributes
+{
+    /**
+     * @param class-string<T> $enumClass
+     */
+    public function __construct(protected string $enumClass)
+    {
+    }
+
+    /**
+     * @return \Illuminate\Support\Collection<T>|null
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): ?Collection
+    {
+        if (empty($attributes[$key])) {
+            return new Collection();
+        }
+
+        $data = Json::decode($attributes[$key]);
+
+        if (!is_array($data)) {
+            throw new LogicException('Invalid data for enum collection cast: ' . $attributes[$key]);
+        }
+
+
+        return (new Collection($data))->map(function($value) {
+            return is_subclass_of($this->enumClass, BackedEnum::class)
+                ? $this->enumClass::from($value)
+                : constant($this->enumClass . '::' . $value);
+        });
+    }
+
+    public function set(Model $model, string $key, mixed $value, array $attributes): array
+    {
+        $valueArray = Collection::wrap($value)
+            ->map(fn($enum) => $this->getStorableEnumValue($enum))
+            ->jsonSerialize();
+
+        return [$key => Json::encode($valueArray)];
+    }
+
+    protected function getStorableEnumValue($enum)
+    {
+        if (is_string($enum) || is_int($enum)) {
+            return $enum;
+        }
+
+        return $enum instanceof BackedEnum ? $enum->value : $enum->name;
+    }
+}
